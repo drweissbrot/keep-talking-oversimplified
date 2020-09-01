c0cec58c1a09c76d37dc4cf16b124d92fa6bcd6c
Pierre Borgmann <drweissbrot@outlook.com>
2020-09-01 03:21:24 +0200

    move from /public to /docs to appease github pages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 const mix = require('laravel-mix')
 
-mix.stylus('stylus/app.styl', 'public/css/app.css')
-.js('js/app.js', 'public/js/app.js')
+mix.stylus('stylus/app.styl', 'docs/css/app.css')
+.js('js/app.js', 'docs/js/app.js')
